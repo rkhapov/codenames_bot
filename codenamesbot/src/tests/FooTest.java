1de9e69bbfacddb0ca20d01973dff9a21837e42d
Roman Khapov <r.khapov@yandex.ru>
2018-12-01 20:01:06 +0500

    initial java project

---
+++ codenamesbot/src/tests/FooTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+  @Test
+  public void myTest() {
+    assertTrue(false);
+  }
+}
