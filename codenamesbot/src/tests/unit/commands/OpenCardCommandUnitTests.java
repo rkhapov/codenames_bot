04d8a8ae249383463dd45da01c47145988e21e30
Roman Khapov <r.khapov@yandex.ru>
2018-12-12 00:46:51 +0500

    wip

--- codenamesbot/src/tests/unit/commands/OpenCardCommandUnitTests.java
+++ codenamesbot/src/tests/unit/commands/OpenCardCommandUnitTests.java
@@ -1,8 +1,21 @@
 package tests.unit.commands;
 
+import static org.mockito.Mockito.mock;
+
+import core.game.server.IGameServer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OpenCardCommandUnitTests {
+
+  private IGameServer server;
+
+  @BeforeEach
+  public void init() {
+    server = mock(IGameServer.class);
+
+
+  }
 
   @Test
   public void execute() {
